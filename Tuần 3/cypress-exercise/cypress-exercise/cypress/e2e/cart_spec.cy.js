76dfeaba860fc1bb09126917831f1c817ad24c86
Trần Quỳnh Anh <tranquynhanh28032016@gmail.com>
2026-01-26 20:41:47 +0700

    #4 check search product

--- Tuần 3/cypress-exercise/cypress-exercise/cypress/e2e/cart_spec.cy.js
+++ Tuần 3/cypress-exercise/cypress-exercise/cypress/e2e/cart_spec.cy.js
@@ -10,4 +10,9 @@ describe('Cart Test', () => {
     cy.get('.inventory_item').first().find('.btn_inventory').click();
     cy.get('.shopping_cart_badge').should('have.text', '1');
   });
+
+  it('Should sort products by price low to high', () => {
+  cy.get('.product_sort_container').select('lohi');
+  cy.get('.inventory_item_price').first().should('have.text', '$7.99');
+});
 });
