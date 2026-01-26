541915ef52c43ee9ec3653a11ce14c4cbfb7d72c
Trần Quỳnh Anh <tranquynhanh28032016@gmail.com>
2026-01-26 20:29:05 +0700

    #1 check login sucessfully

---
+++ Tuần 3/cypress-exercise/cypress-exercise/cypress/e2e/login_spec.cy.js
@@ -0,0 +1,10 @@
+describe('Login Test', () => {
+  it('Should login successfully with valid credentials', () => {
+    cy.visit('https://www.saucedemo.com');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.url().should('include', '/inventory.html');
+  });
+
+});
