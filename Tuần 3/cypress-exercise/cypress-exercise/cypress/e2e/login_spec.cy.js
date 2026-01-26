36d0854ed97d3e1c2b7e319bf70eb3defa6765ff
Trần Quỳnh Anh <tranquynhanh28032016@gmail.com>
2026-01-26 20:39:11 +0700

    #2 check login failed

--- Tuần 3/cypress-exercise/cypress-exercise/cypress/e2e/login_spec.cy.js
+++ Tuần 3/cypress-exercise/cypress-exercise/cypress/e2e/login_spec.cy.js
@@ -6,5 +6,11 @@ describe('Login Test', () => {
     cy.get('#login-button').click();
     cy.url().should('include', '/inventory.html');
   });
-
+  it('Should show error message with invalid credentials', () => {
+  cy.visit('https://www.saucedemo.com');
+  cy.get('#user-name').type('invalid_user');
+  cy.get('#password').type('wrong_password');
+  cy.get('#login-button').click();
+  cy.get('.error-message-container').should('contain', 'Username and password do not match');
+});
 });
