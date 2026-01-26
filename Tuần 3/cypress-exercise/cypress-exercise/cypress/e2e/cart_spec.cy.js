d26594a8516a9ff609f3493311d467a1dcc3906f
Trần Quỳnh Anh <tranquynhanh28032016@gmail.com>
2026-01-26 20:41:09 +0700

    #3 check add to cart

---
+++ Tuần 3/cypress-exercise/cypress-exercise/cypress/e2e/cart_spec.cy.js
@@ -0,0 +1,13 @@
+describe('Cart Test', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+  });
+
+  it('Should add a product to the cart', () => {
+    cy.get('.inventory_item').first().find('.btn_inventory').click();
+    cy.get('.shopping_cart_badge').should('have.text', '1');
+  });
+});
